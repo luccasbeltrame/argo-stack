298d5de050571e235c2e1af1d54a80b641463256
Luccas Beltrame <luccasbeltrame@hotmail.com>
2022-06-26 11:01:32 -0300

    deploy change

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		w.Write([]byte("hello there"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
